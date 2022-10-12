416ab29d2ff922e3962e455e9d4a0f0ab778f1fb
JuveRami <rjuvenaldo@gmail.com>
2022-10-12 13:06:59 +0200

    Ajuste dos conteudos no mobile1

--- assets/js/jar_script.js
+++ assets/js/jar_script.js
@@ -31,6 +31,20 @@ $(document).ready(function(){
     });
 });
 
+$(document).ready(function(){
+  $(".link-easy").on('click', function(event) {
+      if (this.hash !== "") {
+          event.preventDefault();
+          var hash = this.hash;
+          $('html, body').animate({
+              scrollTop: $(hash).offset().top
+          }, 700, function(){
+              window.location.hash = hash;
+          });
+      } 
+  });
+});
+
 $(document).ready(function () {
 
     // $("p").hide();
